bfe017230a0c76e8e0424f4029f71fa0e86b825b
Quentin Ligier <quentin.ligier@bluewin.ch>
2024-06-04 12:21:44 +0200

    Add Gazelle proxy script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # userscripts
 Userscripts created by ahdis
+
+## How to install
+
+You need to install a userscript extension in your browser. Common choices are:
+
+- [ViolentMonkey](https://violentmonkey.github.io/) for Chrome/Chromium, Firefox, Edge.
+- [FireMonkey](https://addons.mozilla.org/en-CA/firefox/addon/firemonkey/) for Firefox.
+- [TamperMonkey](https://www.tampermonkey.net/) for Chrome/Chromium, Edge, Firefox, Safari, Opera.
+
+See also https://github.com/awesome-scripts/awesome-userscripts?tab=readme-ov-file#how-to-use.
+
+## How to modify
+
+When modifying a userscript, you have to change the `@version` field in the header of the script. Otherwise, extensions will not update it.
---
+++ gazelle_proxy.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name        New features for the Gazelle proxy
+// @namespace   ahdis
+// @match       https://gazelle.ihe.net/proxy/messages/http.seam*
+// @grant       none
+// @version     20240604T121900
+// @author      Quentin Ligier
+// @description This script allows to automatically decompress GZIP-encoded messages.
+// @updateURL   https://github.com/ahdis/userscripts/raw/master/gazelle_proxy.user.js
+// ==/UserScript==
+
+const headers = document.querySelector('pre').innerText;
+
+if (headers.includes('Content-Encoding: gzip')) {
+  const downloadButton = document.querySelectorAll('span.gzl-icon-download + input')[1];
+  const largerId = downloadButton.name;
+  const parts = downloadButton.name.split(':');
+  const smallerId = `${parts[0]}:${parts[1]}`;
+
+  const params = new URLSearchParams();
+  params.set(smallerId, smallerId);
+  params.set(largerId, 'Télécharger le fichier');
+  params.set('javax.faces.ViewState', document.querySelector(`form[id="${smallerId}"] input[name="javax.faces.ViewState"]`).value);
+  fetch('https://gazelle.ihe.net/proxy/messages/http.seam', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    body: params
+  }).then(async res => {
+    const ds = new DecompressionStream("gzip");
+    const blob = await res.blob();
+    const decompressedStream = blob.stream().pipeThrough(ds);
+    const decoded = await new Response(decompressedStream).text();
+    const messageDiv = document.querySelector('h3 + div');
+    messageDiv.innerText = `ahdis userscript: the message has been GZIP-decompressed:\n\n${decoded}`;
+  });
+}
